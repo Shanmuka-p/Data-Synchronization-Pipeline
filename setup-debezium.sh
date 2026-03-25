6ff381bd397593fb466afed5f56657a4ad62c421
Shanmuka-p <shanmukareddypadala@gmail.com>
2026-03-25 18:44:32 +0530

    feat(infra): add debezium connector registration script

---
+++ setup-debezium.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Registering Debezium PostgreSQL connector..."
+
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" http://localhost:8083/connectors/ -d '{
+  "name": "products-connector",
+  "config": {
+    "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+    "database.hostname": "postgres",
+    "database.port": "5432",
+    "database.user": "user",
+    "database.password": "password",
+    "database.dbname": "products_db",
+    "topic.prefix": "pg-server",
+    "table.include.list": "public.products",
+    "tombstones.on.delete": "true",
+    "plugin.name": "pgoutput"
+  }
+}'
+
+echo -e "\n\nConnector registration request sent."
